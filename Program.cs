6a71f00d0c33581dbd221241bbc5e0fa9f0c7f96
Wahyu2257 <48467871+Wahyu2257@users.noreply.github.com>
2019-06-25 14:53:55 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arrNama = { "erick", "wely", "sony", "joni" };
+            Console.WriteLine(arrNama[2].ToString() + "\n");
+
+            for (int i = 0; i < arrNama.Length; i++)
+            {
+                Console.WriteLine(arrNama[i]);
+            }
+            Console.WriteLine();
+
+            foreach (string nama in arrNama)
+            {
+                Console.WriteLine(nama);
+            }
+            Console.WriteLine();
+
+            int[] arrBil = new int[3];
+            arrBil[0] = 12;
+            arrBil[1] = 14;
+            arrBil[2] = 24;
+
+            foreach (int bil in arrBil )
+            {
+                Console.WriteLine(bil.ToString());
+            }
+            Console.ReadKey();
+        }
+    }
+}
